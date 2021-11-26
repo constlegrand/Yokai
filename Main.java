e2dbc83d6603310b37483932b88faeb705504a85
BaptisteCl <95075131+BaptisteCl@users.noreply.github.com>
2021-11-26 10:33:15 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,11 +10,19 @@ public class Main {
 
             // Carte 2 couleur
             for (int i=0;i<5;i++){
-                int n = (int)(Math.random() * 4);
+                int n1 = (int)(Math.random() * 4);
+                int n2 = (int)(Math.random() * 4);
                 System.out.println(n);
-                
+                string non=C+string(i)
+                CarteIndice non = new CarteIndice();
+                non.couleur1=n1;
+                non.couleur2=n2;
+                non.couleur3=0;
+                non.couleur4=0;
+                non.etat=False;
+                non.activation=False;
 
-                CarteIndice myCarte = new CarteIndice();
+
             }
         }
 
