cab172c42fa60c26db78a3720d1841440ae49577
BaptisteCl <95075131+BaptisteCl@users.noreply.github.com>
2021-11-26 10:20:39 +0100

    Update CarteIndice.java

--- CarteIndice.java
+++ CarteIndice.java
@@ -3,6 +3,7 @@ public class CarteIndice {
     public int couleur2;
     public int couleur3;
     public boolean etat; // 0 cacher et 1 afficher
+    public activation etat; // 0 nn activer et 1 activer
     public int positionX;
     public int positionY;
     
