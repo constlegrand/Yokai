06b9a61edf828285f95fa0ae42a94dc9d869b6d7
BaptisteCl <95075131+BaptisteCl@users.noreply.github.com>
2021-11-26 10:12:10 +0100

    Update and rename Carte.java to CarteIndice.java

--- Carte.java
+++
@@ -1,4 +0,0 @@
-public class Carte {
-    public 
-    Carte myCarte = new Carte();
-}
---
+++ CarteIndice.java
@@ -0,0 +1,7 @@
+public class CarteIndice {
+    public int couleur1;
+    public int couleur2;
+    public int couleur3;
+    public int etat; // 0 cacher et 1 afficher
+    
+}
