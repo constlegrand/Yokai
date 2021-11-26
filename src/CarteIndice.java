1c9d82e1943f17a4e890ad234ad9d0593e4af955
BaptisteCl <95075131+BaptisteCl@users.noreply.github.com>
2021-11-26 11:08:28 +0100

    premier commit

---
+++ src/CarteIndice.java
@@ -0,0 +1,11 @@
+public class CarteIndice {
+    public int couleur1;
+    public int couleur2;
+    public int couleur3;
+    public int couleur4;  //0 couleur non atribué
+    public boolean etat; // 0 cacher et 1 afficher
+    public boolean activation; // 0 nn activer et 1 activer
+    public int positionX;
+    public int positionY;
+
+}
---
+++ src/CartePerso.java
@@ -0,0 +1,6 @@
+public class CartePerso {
+    public int perso;
+    public boolean etat; // 0 cacher et 1 afficher
+    public int positionX;
+    public int positionY;
+}
