962ff5f968b0025e2623606c4a49646b0123598e
BaptisteCl <95075131+BaptisteCl@users.noreply.github.com>
2021-12-10 08:36:47 +0100

    Update CartePerso.java

--- src/CartePerso.java
+++ src/CartePerso.java
@@ -3,4 +3,34 @@ public class CartePerso {
     public boolean etat; // 0 cacher et 1 afficher
     public int positionX;
     public int positionY;
+
+    // metode tostrign
+    @Override
+    public String toString() {
+        return "CartePerso{" +
+                "perso=" + perso +
+                ", etat=" + etat +
+                ", positionX=" + positionX +
+                ", positionY=" + positionY +
+                '}';
+    }
+
+
+    //constructeur
+    public CartePerso(int perso, boolean etat, int positionX, int positionY){
+        this.perso=perso;
+        this.etat=etat;
+        this.positionX=positionX;
+        this.positionY=positionY;
+    }
+
+    //geteur
+    public int getPerso() {
+        return perso;}
+    public boolean isEtat() {
+        return etat;}
+    public int getPositionX() {
+        return positionX;}
+    public int getPositionY() {
+        return positionY;}
 }
